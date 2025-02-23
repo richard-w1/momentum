ec3338c4793ad2ac5d04613c48a247a30bfb2fc4
Shrestha, Sarthak <sarthakshrestha@my.unt.edu>
2025-02-23 11:29:27 -0600

    added path url for each page

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from website.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('landing/', landing, name = 'landing'),
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('', home_redirect, "Redirection"),
 ]
