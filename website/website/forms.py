778018eb91f27eb232557a6d9ea7639e3e33def5
Wang, Richard <richardwang2@my.unt.edu>
2025-03-01 22:54:40 -0600

    expanded the default user creation form

--- website/website/forms.py
+++ website/website/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from .models import Habit
+from django.contrib.auth.forms import UserCreationForm
+
 
 class HabitForm(forms.ModelForm):
     class Meta:
         model = Habit
         fields = ['name', 'frequency']
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
