b1a5348fde669de50d5f91f7eaa63d121bc811b4
Shrestha, Sarthak <sarthakshrestha@my.unt.edu>
2025-03-16 15:13:43 -0500

    Edited the signup to store birth date in custom_user, updated the profile page to display birth date

--- website/website/forms.py
+++ website/website/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Habit
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import custom_user
 
 class HabitForm(forms.ModelForm):
     class Meta:
@@ -26,5 +27,19 @@ class CustomUserCreationForm(UserCreationForm):
     )
 
     class Meta:
+        #model is user because django's default auth expects a user model
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'birth_date']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+    
+    #creating a instance of custom_user
+    def save(self, commit=True):
+        user = super().save(commit=True)
+        
+        user_profile = custom_user(
+            user=user,
+            birth_date=self.cleaned_data.get('birth_date')
+        )
+        if commit:
+            user_profile.save()
+        
+        return user
