1d91b12eadf07777365816ec0aaf02598e0f323a
ryanstclair1 <ryanhunterstclair23@gmail.com>
2025-04-16 16:26:42 -0500

    Added MEDIA_URL and MEDIA_ROOT for handling uploaded profile pictures

--- website/website/settings.py
+++ website/website/settings.py
@@ -155,3 +155,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'momentumhabitapp@gmail.com'
 EMAIL_HOST_PASSWORD = 'mngj qkfv vyra yzvo'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
