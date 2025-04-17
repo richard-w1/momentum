7ef9261c0f4f59eb8a0105c7ee3af04d6142e2f5
Wang, Richard <richardwang2@my.unt.edu>
2025-04-17 16:52:52 -0500

    Profile picture in navbar dynamically display user uploaded image or default image if none is uploaded.

--- website/website/urls.py
+++ website/website/urls.py
@@ -52,5 +52,6 @@ urlpatterns = [
 ]
 
 # profile pictures
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
